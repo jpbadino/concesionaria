5002cd113a9e5b4ca3197aebba66ffeecf241072
jpbadino <92000500+jpbadino@users.noreply.github.com>
2022-04-25 11:41:08 -0300

    Borro comentarios obsoletos
    El "Camino 2" de este repositorio sigue esa lógica pero archivo por archivo. Referirse al mismo para obtener los resultados.

--- camino-1/app.js
+++ camino-1/app.js
@@ -52,37 +52,4 @@ const concesionaria = {
         return this.autosParaLaVenta().filter(auto => this.puedeComprar(auto,persona))
     }
 
-};
-
-//6)
-
-//7)
-
-//8)
-
-//1) buscarAuto: la funcion buscar autos, recibe la patente como parametro. generamos una variable, la cual le asignamos por defecto el valor null por si la patente no coincide con la del obejto, nos va a retornar el valor inicial. y dsp con el if hacemos la comparacion de patentes y modificamos el valor de autoABuscar con el auto econtrado.
-
-//2) venderAuto: usamos la funcion de buscarAuto para filtrar el objeto, y le cambiamos el valor a la propiedad vendido.
-
-//3) autosParaLaVenta: en autos para la venta filtramos el objeto de autos con el .filter()y este nos va a devolver un auto, que cumpla con la condicion del return. recorda que el "!" es una negación.
-
-//4) autosNuevos: en autos nuevos, usamos la funcionalidad de autosParaLaVenta para filtrar los que esten disponibles para la venta, y hacemos un segundo filter, para filtrar los menores a 100km.
-
-//5) listaDeVentas: filtramos el objeto y devolvemos los autos vendidos (para usar el console.log, cambiar en vendido: false por true para que muestre el precio por consola). de esos autos filtrados, los recorremos con .map() y devolvemos el precio de los autos vendidos.
-
-
-
-//1) console.log(concesionaria.buscarAuto("JJK116"))
-
-//2) console.log(concesionaria.venderAuto("APL123"))
-
-//3) console.log(concesionaria.autosParaLaVenta())
-
-//4) console.log(concesionaria.autosNuevos())
-
-//5) console.log(concesionaria.listaDeVentas())
-
-//6)console.log(concesionaria.totalDeVentas())
-
-
-
+}
